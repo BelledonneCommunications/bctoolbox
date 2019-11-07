c21f30248e3276f02864f1f7ad9d3e2711bec067
François Grisez <francois.grisez@belledonne-communications.com>
2019-11-07 10:15:58 +0100

    Change license date

--- cmake/BcToolboxCMakeUtils.cmake
+++ cmake/BcToolboxCMakeUtils.cmake
@@ -1,6 +1,6 @@
 ############################################################################
 # BcToolboxCMakeUtils.cmake
-# Copyright (C) 2017  Belledonne Communications, Grenoble France
+# Copyright (C) 2010-2019 Belledonne Communications, Grenoble France
 #
 ############################################################################
 #
