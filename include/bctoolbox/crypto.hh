8c724d61442dd5f52508a75af522cfbde8060234
Clemence Him <clemence.him@belledonne-communications.com>
2021-09-10 15:19:05 +0200

    Add templates

--- include/bctoolbox/crypto.hh
+++ include/bctoolbox/crypto.hh
@@ -227,7 +227,13 @@ template <> bool AEADDecrypt<AES256GCM128>(const std::vector<uint8_t> &key, cons
  *
  * @return  The generated derived key
  */
-std::vector<uint8_t> PBKDF2_HMAC_SHA_256(const std::string& password, const std::string& salt, int c, size_t dkLen);
+template <typename hashAlgo>
+std::vector<uint8_t> PBKDF2(const std::string &password, const std::string &salt, int c, size_t dkLen);
+
+/* declare template specialisations */
+extern template std::vector<uint8_t> PBKDF2<SHA256>(const std::string &password, const std::string &salt, int c, size_t dkLen);
+extern template std::vector<uint8_t> PBKDF2<SHA384>(const std::string &password, const std::string &salt, int c, size_t dkLen);
+extern template std::vector<uint8_t> PBKDF2<SHA512>(const std::string &password, const std::string &salt, int c, size_t dkLen);
 
 } // namespace bctoolbox
 #endif // BCTBX_CRYPTO_HH
--- src/crypto/crypto.cc
+++ src/crypto/crypto.cc
@@ -15,6 +15,7 @@ namespace bctoolbox{
  *
  * @return  The xor iterations of chained HMAC-SHA-256 with param c and i
  */
+template<typename hashAlgo>
 static vector<uint8_t> F(const vector<uint8_t>& password, vector<uint8_t> salt, int c, uint32_t i){
     vector<uint8_t> U;
     vector<uint8_t> Ures;
@@ -24,39 +25,40 @@ static vector<uint8_t> F(const vector<uint8_t>& password, vector<uint8_t> salt, 
     for(int k = 3 ; k >= 0 ; k--){
         salt.insert(salt.end(), (uint8_t)i>>(k*8));
     }
-    U = HMAC<bctoolbox::SHA256>(password, salt);
+    U = HMAC<hashAlgo>(password, salt);
     Ures = U;
 
     for(int j = 1 ; j < c ; j++){
-        U = HMAC<bctoolbox::SHA256>(password, U);
+        U = HMAC<hashAlgo>(password, U);
         transform(Ures.begin(), Ures.end(), U.begin(), Ures.begin(), bit_xor<uint8_t>{});
     }
     return Ures;
 }
 
-
-vector<uint8_t> PBKDF2_HMAC_SHA_256(const string& password, const string& salt, int c, size_t dkLen){
+template<typename hashAlgo>
+vector<uint8_t> PBKDF2(const string& password, const string& salt, int c, size_t dkLen){
     vector<uint8_t> P(password.begin(), password.end());
-    vector<uint8_t> S(salt.begin(), salt.end());
+    const vector<uint8_t> S(salt.begin(), salt.end());
     vector<uint8_t> DK{};
     vector<uint8_t> T{};
-    vector<uint8_t>::const_iterator first;
-    vector<uint8_t>::const_iterator last;
 
     uint32_t i = 1;
     while(DK.size() < dkLen){
-        T = F(P, S, c, i);
+        T = F<hashAlgo>(P, S, c, i);
         DK.insert(DK.end(), T.begin(), T.end());
         i++;
     }
 
     bctbx_clean(P.data(), P.size());
-    bctbx_clean(S.data(), S.size());
 
     //Resize DK to dkLen size
     DK.resize(dkLen);
 
     return DK;
 }
+
+template std::vector<uint8_t> PBKDF2<SHA256>(const std::string &password, const std::string &salt, int c, size_t dkLen);
+template std::vector<uint8_t> PBKDF2<SHA384>(const std::string &password, const std::string &salt, int c, size_t dkLen);
+template std::vector<uint8_t> PBKDF2<SHA512>(const std::string &password, const std::string &salt, int c, size_t dkLen);
 
 } // namespace bctoolbox
