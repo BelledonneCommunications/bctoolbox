7facb0c6bcfe7109888ea6d57fafa5cdf7f3408b
Clemence Him <clemence.him@belledonne-communications.com>
2021-09-10 10:48:07 +0200

    Correction namespace in src/crypto.cc

--- src/crypto/crypto.cc
+++ src/crypto/crypto.cc
@@ -1,5 +1,9 @@
 #include <bctoolbox/crypto.hh>
 
+using namespace std;
+
+namespace bctoolbox{
+
 /**
  * @brief Xor iterations of chained HMAC-SHA-256
  *
@@ -10,20 +14,20 @@
  *
  * @return  The xor iterations of chained HMAC-SHA-256 with param c and i
  */
-static std::vector<uint8_t> F(const std::vector<uint8_t>& password,std::vector<uint8_t> salt, int c, uint32_t i){
-    std::vector<uint8_t> U;
-    std::vector<uint8_t> Ures;
+static vector<uint8_t> F(const vector<uint8_t>& password, vector<uint8_t> salt, int c, uint32_t i){
+    vector<uint8_t> U;
+    vector<uint8_t> Ures;
 
     /* for j == 0 */
     //Concatenate salt with i in order of big endian
     for(int k = 3 ; k >= 0 ; k--){
         salt.insert(salt.end(), (uint8_t)i>>(k*8));
     }
-    U = bctoolbox::HMAC<bctoolbox::SHA256>(password, salt);
+    U = HMAC<bctoolbox::SHA256>(password, salt);
     Ures = U;
 
     for(int j = 1 ; j < c ; j++){
-        U = bctoolbox::HMAC<bctoolbox::SHA256>(password, U);
+        U = HMAC<bctoolbox::SHA256>(password, U);
         for(size_t l = 0 ; l < Ures.size() ; l++){
             Ures.at(l) ^= U.at(l);
         }
@@ -32,14 +36,14 @@ static std::vector<uint8_t> F(const std::vector<uint8_t>& password,std::vector<u
 }
 
 
-std::vector<uint8_t> PBKDF2_HMAC_SHA_256(const std::string password, const std::string salt, int c, size_t dkLen){
-    const std::vector<uint8_t> P(password.begin(), password.end());
-    const std::vector<uint8_t> S(salt.begin(), salt.end());
-    std::vector<uint8_t> DK;
-    std::vector<uint8_t> DKres;
-    std::vector<uint8_t> T;
-    std::vector<uint8_t>::const_iterator first;
-    std::vector<uint8_t>::const_iterator last;
+vector<uint8_t> PBKDF2_HMAC_SHA_256(const string password, const string salt, int c, size_t dkLen){
+    const vector<uint8_t> P(password.begin(), password.end());
+    const vector<uint8_t> S(salt.begin(), salt.end());
+    vector<uint8_t> DK;
+    vector<uint8_t> DKres;
+    vector<uint8_t> T;
+    vector<uint8_t>::const_iterator first;
+    vector<uint8_t>::const_iterator last;
     //Output size of the HMAC-SHA-256 function
     size_t hLen = 32;
     uint32_t stop = (uint32_t)(dkLen/hLen);
@@ -63,3 +67,5 @@ std::vector<uint8_t> PBKDF2_HMAC_SHA_256(const std::string password, const std::
 
     return DKres;
 }
+
+} // namespace bctoolbox
